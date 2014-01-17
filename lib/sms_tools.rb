db7d85190dca82966efd267c255769380ec26965
Dimitar Dimitrov <me@ddimitrov.name>
2014-01-17 19:01:58 +0200

    Make the gem a Rails engine (only if used in Rails)

--- lib/sms_tools.rb
+++ lib/sms_tools.rb
@@ -1,3 +1,7 @@
 require 'sms_tools/version'
 require 'sms_tools/encoding_detection'
 require 'sms_tools/gsm_encoding'
+
+if defined?(::Rails) and ::Rails.version >= '3.1'
+  require 'sms_tools/rails/engine'
+end
